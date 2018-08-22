4d30be5837797deaf032eaefb65b836a339b83af
edasilvamendes <=>
2018-08-22 10:11:38 +0200

    Initial commit

---
+++ database/migrations/2018_08_21_133242_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('name', 100);
+
+            //Contraintes via la table "posts" (clé etrangere)
+            /*$table->unsignedInteger('id_posts');
+            $table->foreign('id_posts')->references('id')->on('posts')->onDelete('cascade');*/
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ database/migrations/2018_08_22_075549_create_posts_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->enum('post_type', ['formation', 'stage'])->default('formation'); // check
+            $table->string('title', 100); // varchar
+            $table->longText('description');
+            $table->dateTime('begin_date');
+            $table->dateTime('end_date');
+            $table->float('price');
+            $table->integer('max_students');
+
+
+            //OTHER SOLUTION
+            $table->unsignedInteger('id_category');
+            $table->foreign('id_category')->references('id')->on('categories')->onDelete('cascade');
+
+            $table->unsignedInteger('id_picture')->nullable();
+            $table->foreign('id_picture')->references('id')->on('pictures')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
